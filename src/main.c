5fdfa53f9b2da76bf23d4b5774a50d828c1b8d87
Sebastian Riedel <git@basxto.de>
2020-12-12 11:49:34 +0100

    Generate starting tile

--- src/main.c
+++ src/main.c
@@ -9,8 +9,12 @@
 #define FONT_START (48)
 #define FONT_ASCII (FONT_START-48)
 #define FONT_HEX   (FONT_START+7)
+#define map_width  (10)
+#define map_size   (8*map_width)
 
 uint16_t seed;
+uint8_t overworld[map_size];
+uint8_t tmp_tile[4];
 
 void init_screen() {
     HIDE_BKG;
@@ -30,26 +34,67 @@ void init_screen() {
     DISPLAY_ON;
 }
 
+void write_hex(uint8_t x, uint8_t y, uint8_t num){
+    uint8_t hex[2];
+    hex[0] = FONT_HEX + (num >> 4);
+    hex[1] = FONT_HEX + (num & 0xF);
+    set_win_tiles(x, y, 2, 1, hex);
+}
+
 void roll_seed(){
-    uint16_t seed = LY_REG;
+    seed = LY_REG;
     seed |= (uint16_t)DIV_REG << 8;
-    unsigned char num []= {(uint8_t)(seed & 0xF)+FONT_HEX, (uint8_t)((seed>>4) & 0xF)+FONT_HEX, (uint8_t)((seed>>8) & 0xF)+FONT_HEX, (uint8_t)((seed>>12) & 0xF)+FONT_HEX};
-    set_win_tiles(11, 7, 4, 1, num);
+    write_hex(11, 7, (uint8_t)(seed>>8));
+    write_hex(13, 7, (uint8_t)(seed));
+}
+
+void draw_overworld(){
+    uint8_t x = 0;
+    uint8_t y = 0;
+    for(uint8_t i = 0; i < map_size; ++i){
+        tmp_tile[0] = PATH_START + overworld[i]*4;
+        tmp_tile[2] = tmp_tile[0]+1;
+        tmp_tile[1] = tmp_tile[2]+1;
+        tmp_tile[3] = tmp_tile[1]+1;
+        set_bkg_tiles(x, y, 2, 2, tmp_tile);
+        x += 2;
+        if(x >= (map_width*2)){
+            x = 0;
+            y += 2;
+        }
+    }
+}
+
+void generate_overworld(){
+    // reset path
+    for(uint8_t i = 0; i < map_size; ++i){
+        overworld[i] = 0;
+    }
+    uint8_t start = arand() & 0x3F;// 16 tiles are left out (%64) map_size;
+    overworld[start] = 2;
+    write_hex(16, 0, start);
+    write_hex(18, 0, overworld[start]);
 }
 
 void generate_map(){
     // initialize random numbers
     initarand(seed);
+    generate_overworld();
 }
 
 void main() {
     init_screen();
-    waitpad(0xFF);
-    waitpadup();
-    SHOW_WIN;
-    roll_seed();
-    set_win_tiles(5, 9,  1, 1, "3");
-    waitpad(0xFF);
-    waitpadup();
-    generate_map();
+    while(1){
+        waitpad(0xFF);
+        waitpadup();
+        move_win(7, 0);
+        SHOW_WIN;
+        roll_seed();
+        set_win_tiles(5, 9,  1, 1, "3");
+        waitpad(0xFF);
+        waitpadup();
+        generate_map();
+        draw_overworld();
+        move_win(7, 17*8);
+    }
 }
