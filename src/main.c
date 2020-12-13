cf772e6f1d0c1b908edbc44706ac6dbe1ec40e90
Sebastian Riedel <git@basxto.de>
2020-12-13 06:11:14 +0100

    Draw first buggy street

--- src/main.c
+++ src/main.c
@@ -5,15 +5,21 @@
 #include "../build/squont8ng_micro_2bpp.c"
 #include "../build/blowharder_path_2bpp.c"
 
-#define PATH_START (128)
-#define FONT_START (48)
-#define FONT_ASCII (FONT_START-48)
-#define FONT_HEX   (FONT_START+7)
-#define map_width  (10)
-#define map_size   (8*map_width)
+#define u8(x)      (uint8_t)(x)
+#define PATH_START u8(128)
+#define FONT_START u8(48)
+#define FONT_ASCII u8(FONT_START-48)
+#define FONT_HEX   u8(FONT_START+7)
+#define map_width  u8(10)
+#define map_size   u8(8*map_width)
+#define dir_E      u8(1<<0)
+#define dir_S      u8(1<<1)
+#define dir_W      u8(1<<2)
+#define dir_N      u8(1<<3)
 
 uint16_t seed;
 uint8_t overworld[map_size];
+uint8_t backtrack[map_size];
 uint8_t tmp_tile[4];
 
 void init_screen() {
@@ -70,10 +76,70 @@ void generate_overworld(){
     for(uint8_t i = 0; i < map_size; ++i){
         overworld[i] = 0;
     }
-    uint8_t start = arand() & 0x3F;// 16 tiles are left out (%64) map_size;
-    overworld[start] = 2;
-    write_hex(16, 0, start);
-    write_hex(18, 0, overworld[start]);
+    uint8_t try = 0;
+    uint8_t visited = 1;
+    uint8_t tile = arand() & 0x3F;// 16 tiles are left out (%64) map_size;
+    // 0: E, 1: S, 2: W, 3: N
+    uint8_t direction;// = 1<<(arand() & 0x3);
+    //overworld[tile] = direction;
+    //write_hex(16, 0, tile);
+    //write_hex(18, 0, overworld[tile]);
+    do{
+        uint8_t next_tile = tile;
+        try = 0;
+        direction = 1<<(arand() & 0x3);
+        write_hex(8, 0, overworld[tile]);
+        write_hex(11, 0, direction);
+        // check if direction is free
+        // rotate otherwise
+        // also calculate next tile
+        do{
+            if(direction == dir_E){
+                // is that direction free && is it within map bounds?
+                if(!(overworld[tile] & dir_E) && (tile%map_width) != map_width-1){
+                    ++next_tile;
+                    break;
+                }
+            }else if(direction == dir_W){
+                if(!(overworld[tile] & dir_W) && (tile%map_width) != 0){
+                    --next_tile;
+                    break;
+                }
+            }else if(direction == dir_N){
+                if(!(overworld[tile] & dir_N) && tile < map_width){
+                    next_tile -= map_width;
+                    break;
+                }
+            }else if(!(overworld[tile] & dir_S) && tile < (map_size - map_width)){
+                next_tile += map_width;
+                break;
+            }
+            // circular shift
+            direction = ((direction << 1) | (direction >> 3)) & 0xF;
+            ++try;
+        }while(try < 4);
+        if(try >= 4){
+            //nothing was free :/
+            break;
+        }
+        // actually draw path
+        overworld[tile] |= direction;
+        //i = ((i << 4) | (i >> 4));
+        direction = ((direction << 2) | (direction >> 2)) & 0xF;
+        write_hex(13, 0, direction);
+        // swap nibbles
+        overworld[next_tile] |= direction;// |= u8( u8(direction & u8(0x0F)) << u8(4) | u8(direction & u8(0xF0)) >> u8(4) );
+        //TODO: remove this debug stuff
+        write_hex(16, 0, tile);
+        write_hex(18, 0, next_tile);
+        //draw_overworld();
+        //waitpad(0xFF);
+        //waitpadup();
+        next_tile = tile;
+        ++visited;
+        if(visited >= map_size)
+            break;
+    }while(visited);
 }
 
 void generate_map(){
